efa76de6676843f0a8fb9767eb60d1f61e3c50c3
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-23 13:09:33 +0300

    Project set up

---
+++ src/main/java/com/workintech/burger/demo/BurgerspringApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.burger.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/burger/demo/BurgerspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.burger.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
